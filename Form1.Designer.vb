c3979413eb75eba91a283bcfdf51a1936613c06d
Asus <Asus@LAPTOP-D800TFOG>
2024-03-08 20:17:13 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.AttachButton = New System.Windows.Forms.Button()
+        Me.MessageTextBox = New System.Windows.Forms.TextBox()
+        Me.send = New System.Windows.Forms.Button()
+        Me.ChatBox = New System.Windows.Forms.TextBox()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.connect = New System.Windows.Forms.Button()
+        Me.clientport = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.start = New System.Windows.Forms.Button()
+        Me.ServerPort = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker2 = New System.ComponentModel.BackgroundWorker()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel3.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), System.Drawing.Image)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.AttachButton)
+        Me.Panel1.Controls.Add(Me.MessageTextBox)
+        Me.Panel1.Controls.Add(Me.send)
+        Me.Panel1.Controls.Add(Me.ChatBox)
+        Me.Panel1.Controls.Add(Me.Panel3)
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(800, 450)
+        Me.Panel1.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(361, 319)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'AttachButton
+        '
+        Me.AttachButton.Location = New System.Drawing.Point(671, 345)
+        Me.AttachButton.Name = "AttachButton"
+        Me.AttachButton.Size = New System.Drawing.Size(108, 58)
+        Me.AttachButton.TabIndex = 5
+        Me.AttachButton.Text = "Files"
+        Me.AttachButton.UseVisualStyleBackColor = True
+        '
+        'MessageTextBox
+        '
+        Me.MessageTextBox.Location = New System.Drawing.Point(565, 201)
+        Me.MessageTextBox.Multiline = True
+        Me.MessageTextBox.Name = "MessageTextBox"
+        Me.MessageTextBox.Size = New System.Drawing.Size(214, 90)
+        Me.MessageTextBox.TabIndex = 4
+        '
+        'send
+        '
+        Me.send.Location = New System.Drawing.Point(563, 345)
+        Me.send.Name = "send"
+        Me.send.Size = New System.Drawing.Size(108, 58)
+        Me.send.TabIndex = 4
+        Me.send.Text = "send"
+        Me.send.UseVisualStyleBackColor = True
+        '
+        'ChatBox
+        '
+        Me.ChatBox.Location = New System.Drawing.Point(12, 161)
+        Me.ChatBox.Multiline = True
+        Me.ChatBox.Name = "ChatBox"
+        Me.ChatBox.Size = New System.Drawing.Size(508, 277)
+        Me.ChatBox.TabIndex = 3
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.connect)
+        Me.Panel3.Controls.Add(Me.clientport)
+        Me.Panel3.Controls.Add(Me.Label2)
+        Me.Panel3.Controls.Add(Me.TextBox5)
+        Me.Panel3.Location = New System.Drawing.Point(12, 85)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(776, 70)
+        Me.Panel3.TabIndex = 2
+        '
+        'connect
+        '
+        Me.connect.Location = New System.Drawing.Point(680, 32)
+        Me.connect.Name = "connect"
+        Me.connect.Size = New System.Drawing.Size(75, 23)
+        Me.connect.TabIndex = 4
+        Me.connect.Text = "connect"
+        Me.connect.UseVisualStyleBackColor = True
+        '
+        'clientport
+        '
+        Me.clientport.Location = New System.Drawing.Point(338, 33)
+        Me.clientport.Name = "clientport"
+        Me.clientport.Size = New System.Drawing.Size(321, 22)
+        Me.clientport.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Client"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(6, 33)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(307, 22)
+        Me.TextBox5.TabIndex = 3
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.start)
+        Me.Panel2.Controls.Add(Me.ServerPort)
+        Me.Panel2.Controls.Add(Me.TextBox2)
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Location = New System.Drawing.Point(12, 12)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(776, 67)
+        Me.Panel2.TabIndex = 1
+        '
+        'start
+        '
+        Me.start.Location = New System.Drawing.Point(680, 32)
+        Me.start.Name = "start"
+        Me.start.Size = New System.Drawing.Size(75, 23)
+        Me.start.TabIndex = 3
+        Me.start.Text = "Start"
+        Me.start.UseVisualStyleBackColor = True
+        '
+        'ServerPort
+        '
+        Me.ServerPort.Location = New System.Drawing.Point(338, 33)
+        Me.ServerPort.Name = "ServerPort"
+        Me.ServerPort.Size = New System.Drawing.Size(321, 22)
+        Me.ServerPort.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 33)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(307, 22)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Server"
+        '
+        'BackgroundWorker2
+        '
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents MessageTextBox As TextBox
+    Friend WithEvents send As Button
+    Friend WithEvents ChatBox As TextBox
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents connect As Button
+    Friend WithEvents clientport As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents start As Button
+    Friend WithEvents ServerPort As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BackgroundWorker2 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents AttachButton As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,117 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Drawing
+
+Public Class Form1
+    Private client As TcpClient
+    Public STR As StreamReader
+    Public STW As StreamWriter
+    Public recieve As String
+    Public TextToSend As String
+    Public ImageToSend As Image
+
+    Private Sub AttachButton_Click(sender As Object, e As EventArgs) Handles AttachButton.Click
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "Image Files (*.bmp;*.jpg;*.jpeg;*.png;*.gif)|*.bmp;*.jpg;*.jpeg;*.png;*.gif"
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim imageFilePath As String = openFileDialog.FileName
+            ImageToSend = Image.FromFile(imageFilePath)
+            PictureBox1.Image = ImageToSend
+        End If
+    End Sub
+
+    Private Sub start_Click(sender As Object, e As EventArgs) Handles start.Click
+        Dim listener As New TcpListener(IPAddress.Any, Integer.Parse(ServerPort.Text))
+        listener.Start()
+        client = listener.AcceptTcpClient()
+        STR = New StreamReader(client.GetStream())
+        STW = New StreamWriter(client.GetStream())
+        STW.AutoFlush = True
+
+        BackgroundWorker1.RunWorkerAsync()
+        BackgroundWorker2.WorkerSupportsCancellation = True
+    End Sub
+
+    Private Sub connect_Click(sender As Object, e As EventArgs) Handles connect.Click
+        client = New TcpClient()
+        Dim IpEnd As New IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234)
+        Try
+            client.Connect(IpEnd)
+            If (client.Connected) Then
+                ChatBox.AppendText("Connected to server " + Environment.NewLine)
+                STW = New StreamWriter(client.GetStream())
+                STR = New StreamReader(client.GetStream())
+                STW.AutoFlush = True
+                BackgroundWorker1.RunWorkerAsync()
+                BackgroundWorker2.WorkerSupportsCancellation = True
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+        End Try
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        While (client.Connected)
+            Try
+                recieve = STR.ReadLine
+                If recieve.StartsWith("IMAGE:") Then
+                    Dim imageData As Byte() = Convert.FromBase64String(recieve.Substring(6))
+                    Using ms As New MemoryStream(imageData)
+                        Dim receivedImage As Image = Image.FromStream(ms)
+                        Me.ChatBox.Invoke(Sub() AppendImage(receivedImage, "You"))
+                    End Using
+                Else
+                    Me.ChatBox.Invoke(Sub() AppendText("You: " + recieve))
+                End If
+                recieve = ""
+            Catch ex As Exception
+                MessageBox.Show(ex.Message.ToString)
+            End Try
+        End While
+    End Sub
+
+    Private Sub BackgroundWorker2_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker2.DoWork
+        If client IsNot Nothing AndAlso client.Connected Then
+            If ImageToSend IsNot Nothing Then
+                Using ms As New MemoryStream()
+                    ImageToSend.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg)
+                    Dim imageData As Byte() = ms.ToArray()
+                    Dim imageString As String = "IMAGE:" + Convert.ToBase64String(imageData)
+                    STW.WriteLine(imageString)
+                    Me.ChatBox.Invoke(Sub() AppendImage(ImageToSend, "ME"))
+                End Using
+            Else
+                If TextToSend <> "" Then
+                    STW.WriteLine(TextToSend)
+                    Me.ChatBox.Invoke(Sub() AppendText("ME: " + TextToSend))
+                End If
+            End If
+        Else
+            MessageBox.Show("Sending Failed")
+        End If
+        BackgroundWorker2.CancelAsync()
+    End Sub
+
+    Private Sub send_Click(sender As Object, e As EventArgs) Handles send.Click
+        If MessageTextBox.Text <> "" Then
+            TextToSend = MessageTextBox.Text
+            BackgroundWorker2.RunWorkerAsync()
+        End If
+        MessageTextBox.Text = ""
+    End Sub
+
+    Private Sub AppendText(ByVal text As String)
+        ChatBox.AppendText(text + Environment.NewLine)
+    End Sub
+
+    Private Sub AppendImage(ByVal image As Image, ByVal senderName As String)
+        ChatBox.AppendText(senderName & ": [Image Sent]" + Environment.NewLine)
+        ChatBox.SelectionStart = ChatBox.TextLength
+        ChatBox.ReadOnly = False
+
+        Clipboard.SetImage(image)
+        ChatBox.Paste()
+        ChatBox.ReadOnly = True
+    End Sub
+End Class
